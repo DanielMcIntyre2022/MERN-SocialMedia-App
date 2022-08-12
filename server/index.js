4210173df7b4110569be273c0dfa18a726558fca
Daniel <daniel-mcintyre@hotmail.com>
2022-08-12 15:20:03 -0400

    updates to server backend setup

--- server/index.js
+++ server/index.js
@@ -2,18 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/posts', postRoutes)
 
 const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PSW}@cluster0.omkokcu.mongodb.net/?retryWrites=true&w=majority`
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(connectionStr, { useNewUrlParser: true }).then(() => console.log('connected to mongoDB')).catch(err => console.log(err)); 
+mongoose.connect(connectionStr, { useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err)); 
 
-app.listen(() => {
-    console.log(`Server running on port ${PORT}`);
-})
