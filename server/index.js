45f17f63e691ea94c23673707228bb7c8182844b
Daniel <daniel-mcintyre@hotmail.com>
2022-08-12 12:42:31 -0400

    initial backend setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const CONNECTION_URL = process.env.ATLAS_URI;
