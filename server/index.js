c7abb61251fe8f408f93b12afe6b4905a63d880d
Daniel <daniel-mcintyre@hotmail.com>
2022-08-12 14:24:45 -0400

    initial backend server setup and connection to MONGODB

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -7,4 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const CONNECTION_URL = process.env.ATLAS_URI;
+const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PSW}@cluster0.omkokcu.mongodb.net/?retryWrites=true&w=majority`
+
+const PORT = 5000;
+
+mongoose.connect(connectionStr, { useNewUrlParser: true }).then(() => console.log('connected to mongoDB')).catch(err => console.log(err)); 
+
+app.listen(() => {
+    console.log(`Server running on port ${PORT}`);
+})
